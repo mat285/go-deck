b9fdc8b61ae8936a90f3557e2386dcfa8a531f84
michaelt <michaelt@blend.com>
2017-12-08 00:38:37 -0800

    Some updates

--- util.go
+++ util.go
@@ -12,3 +12,16 @@ func Contains(cards []*Card, card *Card) bool {
 	}
 	return false
 }
+
+// ContainsValue returns if the slice contains a card with the value
+func ContainsValue(cards []*Card, card *Card) bool {
+	if card == nil {
+		return false
+	}
+	for _, c := range cards {
+		if c.Value == card.Value {
+			return true
+		}
+	}
+	return false
+}
